429786c8158277cb897cddd62991834bc746cbaf
jana <bkeyh382@gmail.com>
2026-04-12 14:10:49 +0300

    Add function to sum two float numbers

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that provides a function to add two float numbers."""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floats and return their sum as a float."""
+    return a + b
