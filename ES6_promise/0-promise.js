8422f15f7be784797a9a2a1706f59f2d4d323dd2
jana <bkeyh382@gmail.com>
2026-05-11 18:47:13 +0300

    Create getResponseFromAPI function that returns a Promise

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+/**
+ * Returns a Promise.
+ * @returns {Promise} A new promise object.
+ */
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+
+    resolve();
+  });
+}
